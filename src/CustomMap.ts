eed3650950806f637b25314faae041a8747349e9
vitaliy <Vitalij.Urjashnik@privatbank.ua>
2021-10-06 21:57:06 +0300

    add custom map

---
+++ src/CustomMap.ts
@@ -0,0 +1,10 @@
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(mapDivId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(mapDivId), {
+      zoom: 1,
+      center: { lat: 0, lng: 0 },
+    });
+  }
+}
