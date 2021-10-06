945edc953ae4f6ce1f23cb2643d153859c6587be
vitaliy <Vitalij.Urjashnik@privatbank.ua>
2021-10-06 22:51:22 +0300

    create universal method addMarker

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,6 @@
+import { User } from './User';
+import { Company } from './Company';
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -5,6 +8,13 @@ export class CustomMap {
     this.googleMap = new google.maps.Map(document.getElementById(mapDivId), {
       zoom: 1,
       center: { lat: 0, lng: 0 },
+    });
+  }
+
+  addMarker(mappable: User | Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: { lat: mappable.location.lat, lng: mappable.location.lng },
     });
   }
 }
